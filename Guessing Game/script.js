6d614d1f5bf07c6570901933befdb29839330b50
Saj <56212280+SLKGill@users.noreply.github.com>
2021-05-10 19:51:22 -0400

    Add files via upload
    Adding an attempts pane to show number of guesses it takes for user to get right answer

--- Guessing Game/script.js
+++ Guessing Game/script.js
@@ -1,12 +1,17 @@
-/*let btn = document.getElementById('btn');
+let btn = document.getElementById('btn');
 let output = document.getElementById("outputtext");
 
 let number = [Math.floor(Math.random()*100)];
 
+let attempts = document.getElementById('attempts').value = 0;
+
 btn.addEventListener('click', function(){
-  let input = document.getElementById('userInput').value;
+  attempts++;
+  //console.log(attempts)
+  document.getElementById('attempts').value= attempts; 
+  let input = document.getElementById('userinput').value;
   if(input==number){
-    output.innerHTML = 'You guessed right, your number was ${number}'
+    output.innerHTML = `You guessed right, your number was ${number}`
   }
   else if(input < number){
     output.innerHTML = "You guessed too low!"
@@ -14,22 +19,4 @@ btn.addEventListener('click', function(){
   if(input>number){
     output.innerHTML = "You guessed too high"
   };
-});*/
-
-let btn = document.getElementById('btn');
-let output = document.getElementById('outputtext');
-
-let number = [Math.floor(Math.random() * 100)];
-
-
-btn.addEventListener('click', function(){
-    let input = document.getElementById('userinput').value;
-    if (input == number){
-        output.innerHTML = `You guessed right, your number was ${number}`
-    } else if (input < number){
-        output.innerHTML = "You guessed too low!"
-    };
-    if (input > number){
-        output.innerHTML = "you guessed too high!"
-    };
 });
